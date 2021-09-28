f0392ecc943f0975da1cc9a758d35c7909f23212
Asher Sandler <52999326+asher-sandler@users.noreply.github.com>
2021-09-28 23:10:22 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+const el = (
+  <div>
+    <h1>Hello There All</h1>
+    <h2>My name is Asher Sandler</h2>
+  </div>
+);
+
+ReactDom.render(el, document.getElementById('root'));
